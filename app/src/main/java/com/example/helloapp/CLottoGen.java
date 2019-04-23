2fe72c5dc5807728cc1a5569755ed43ade68dff6
KCF4ng <kcf4ng@gmail.com>
2019-04-23 17:44:51 +0800

    #樂透隨機數字

--- app/src/main/java/com/example/helloapp/CLottoGen.java
+++ app/src/main/java/com/example/helloapp/CLottoGen.java
@@ -11,7 +11,7 @@ public class CLottoGen {
         for(int i =1 ; i<50 ; i++){
             list.add(Integer.valueOf(i));
         }
-
+        //把1~49加進去list裡面_END
 
         //取六個號碼
         int[] nums = new int[6];
@@ -20,6 +20,22 @@ public class CLottoGen {
             nums[i] = Integer.valueOf(list.get(index));
             list.remove(index);
         }
+        //取六個號碼_END
+
+
+        //排序:泡沫排序法(Bubble Sort)
+        for(int i = 0 ;  i <nums.length; i++){
+            for(int j =0; j<nums.length-1;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        }
+        //排序_End
+
+
         String s = "";
         for(int i  : nums ){
           s += String.valueOf(i)+" ";
